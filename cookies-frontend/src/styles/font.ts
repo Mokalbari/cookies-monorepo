0d68bbc6cf7d829efa5ae2d75af68be0f5d4c9eb
mokalbari <150693283+Mokalbari@users.noreply.github.com>
2025-03-15 10:50:52 +0100

    add fonts, metadata, ico

--- cookies-frontend/src/styles/font.ts
+++ cookies-frontend/src/styles/font.ts
@@ -1,4 +1,6 @@
 import { Bebas_Neue, Finger_Paint } from "next/font/google"
 
 export const bebasNeue = Bebas_Neue({ weight: "400" })
-export const fingerPaint = Finger_Paint({ weight: "400" })
+export const fingerPaint = Finger_Paint({
+  weight: "400",
+})
