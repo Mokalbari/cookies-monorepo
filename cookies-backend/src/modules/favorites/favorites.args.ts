e6b30e1dfea62177e6e14b5804e8e0b28e164110
mokalbari <150693283+Mokalbari@users.noreply.github.com>
2025-03-19 22:39:09 +0100

    restart backend from fresh

--- cookies-backend/src/modules/favorites/favorites.args.ts
+++
@@ -1,8 +0,0 @@
-import { ArgsType, Field } from '@nestjs/graphql';
-import { PaginatedEntity } from 'src/common/entities/paginated.entity';
-
-@ArgsType()
-export class GetFavoritesArgs extends PaginatedEntity {
-  @Field()
-  userId: string;
-}
